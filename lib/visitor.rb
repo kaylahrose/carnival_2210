221829afc74abcfbcf6af0078e51971e80a47302
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-14 08:07:06 -0800

    Creat visitor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,10 @@
 class Visitor
+    attr_reader :name, :height, :spending_money, :preferences
+    def initialize(name, height, money)
+        @name = name
+        @height = height
+        @spending_money = (money.delete'$').to_i
+        @preferences = []
+    end
 
 end
